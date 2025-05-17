6aa44d14ab7df7e617bf58b64a8436389883f9f4
Aman Gupta <ag597482@gmail.com>
2025-05-17 13:53:22 +0530

    initial commit

---
+++ src/main/java/com/indra/co/NearBy_Pro_Backend/NearByProBackendApplication.java
@@ -0,0 +1,13 @@
+package com.indra.co.NearBy_Pro_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NearByProBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NearByProBackendApplication.class, args);
+	}
+
+}
